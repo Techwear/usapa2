843fcf324a4097782ceebc7d44675f6654625230
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-18 03:07:25 +1100

    finished build_kernel script

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,16 +12,23 @@ echo "[SCRIPT] Cloning Linux kernel."
 git clone --depth=1 https://github.com/raspberrypi/linux
 
 #Get .config file from github and move it into linux folder
-echo "[SCRIPT] Downloading .config file from github"
+echo "[SCRIPT] Checkout .config file from github"
 wget https://raw.githubusercontent.com/Techwear/usapa2/master/.config
-echo "[SCRIPT] Moving .config into linux folder"
+echo "[SCRIPT] Move .config to linux folder"
 sudo cp .config linux/
 
-#Move into linux folder and compile?
-
-#Copy Files
-
+#Compile and copy files
+cd linux/
+echo "[SCRIPT] Compiling kernel"
+make -j4 zImage modules dtbs
+echo "[SCRIPT] Copying files to final locations"
+sudo make modules_install
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+sudo cp arch/arm/boot/zImage /boot/$KERNEL.img
 
 #Reboot
-
+echo "[SCRIPT] Rebooting Raspberry Pi"
+sudo reboot
 
