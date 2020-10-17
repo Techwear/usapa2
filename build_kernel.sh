145525bb0a4cc2b5f8d104f88488eca2115c0d8a
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-17 23:38:01 +1100

    able to access .config from repo

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,16 +1,26 @@
+
 #!/bin/bash
+#Variables
+kernel_path=linux/
 
 #Installing prerequisite packages, script user must run script with sudo so the script doesn't ask for a password
 echo "[SCRIPT] Installing prerequisites for kernel."
 sudo apt install raspberrypi-kernel-headers build-essential bc git wget bison flex libssl-dev make
 
-#Install kernel resources
+#Clone the Linux kernel
+echo "[SCRIPT] Cloning Linux kernel."
+git clone --depth=1 https://github.com/raspberrypi/linux
 
-#Generate kernel config
+#Get .config file from github and move it into linux folder
+echo "[SCRIPT] Downloading .config file from github"
+wget https://raw.githubusercontent.com/Techwear/usapa2/master/.config
+echo "[SCRIPT] Moving .config into linux folder"
+sudo cp .config linux/
 
-#Get .config file from github
+#Move into linux folder and compile?
 
 #Copy Files
+
 
 #Reboot
 
