0c79860f647d84bd4b8b012945aa3de5f2a9a815
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-17 21:19:28 +1100

    planned out script 1

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-#installing prerequisite packages assuming user has sudo 
+
+#Installing prerequisite packages, script user must run script with sudo so the script doesn't ask for a password
 echo "[SCRIPT] Installing prerequisites for kernel."
 sudo apt install raspberrypi-kernel-headers build-essential bc git wget bison flex libssl-dev make
+
+#Install kernel resources
+
+#Generate kernel config
+
+#Get .config file from github
+
+#Copy Files
+
+#Reboot
+
+
