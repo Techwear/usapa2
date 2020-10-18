8e5c9d3f382ff2dbb01497ff790001be62f05501
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-18 17:48:21 +1100

    planned script 3

---
+++ performance_monitor_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Script that changes the flicker of the green LED light based on cpu load
+
+#Read CPU temp into a percentage
+#CPUtempPercent=x
+#sec=1
+
+#Divide time by cpu temp  for light
+#expr $sec / $CPUtempPercent = time
+
+#run light()
+#command to turn on green light by time
