4a2b0b25bc1028769bcff490e74d62d8bf880408
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-18 14:36:01 +1100

    added temp and version cmd to performance script

--- performance_monitor.sh
+++ performance_monitor.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+#Get information about the performance of my pi while compling kernel
+
+#Version
+echo "[SCRIPT] Version of VideoCore firmware" 
+vcgencmd version
+echo " "
+
+#Temperature of Pi
+echo "[SCRIPT] Temperature of Pi"
+vcgencmd measure_temp
