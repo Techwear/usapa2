06b28c1d0f67349fb6f834abd6d71d78fc703c95
Christian Phongsavan <s3781816@student.rmit.edu.au>
2020-10-18 15:23:24 +1100

    added on of the info i want to read

--- performance_monitor.sh
+++ performance_monitor.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 #Get information about the performance of my pi while compling kernel
+#Need to run this script before it compling on first script
 
 #Version
 echo "[SCRIPT] Writing version of VideoCore firmware to file" 
 vcgencmd version >> kernel_performance_data 
 echo " "
-
 #Temperature of Pi
 echo "[SCRIPT] Writing temperature of Pi to file"
 vcgencmd measure_temp >> kernel_performance_data
+echo " "
+echo "[SCRIPT] Writing throttled state to file"
+vcgencmd get_throttled  >> kernel_performance_data
+echo " "
+echo "[SCRIPT] Writing memory split between CPU and GPU"
+vcgencmd get_mem arm >> kernel_performance_data
+vcgencmd get_mem gpu >> kernel_performance_data
+echo " "
+echo "[SCRIPT] Writing CPU clock frequency to file"
+vcgencmd measure_clock arm >> kernel_performance_data
+
+#STOP SCRIPT WHEN USR1 SIGNAL IS RECIEVED
+ 
